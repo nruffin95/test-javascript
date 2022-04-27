c3145f363df5343a2ea8dd15584aa2d229746a98
nruffin95 <nruffin95@gmail.com>
2022-04-27 18:54:26 -0500

    added styles to picture in JS to center

--- test/js/script.js
+++ test/js/script.js
@@ -3,6 +3,7 @@ const getIt = document.getElementById('get')
 const gender = document.getElementById('gender')
 const activity = document.getElementById('activity')
 const button = document.getElementById('button1')
+const picture = document.getElementById('picture')
 
     const getRandomUserData = () => {
         
@@ -13,7 +14,15 @@ const button = document.getElementById('button1')
             const firstName = data.results[0].name.first
             const lastName = data.results[0].name.last
             const cityLocation = data.results[0].location.city
-            const  country = data.results[0].location.country
+            const country = data.results[0].location.country
+            const randomPicture = data.results[0].picture.large
+
+            picture.innerHTML = `<img src="${randomPicture}" alt="" id="picture"></img>`
+            picture.style.display = "flex";
+            picture.style.alignItems = "center";
+            picture.style.justifyContent = "center";
+            picture.style.paddingTop = "10px";
+
             pushIt.innerHTML = firstName + " " + lastName
             getIt.innerHTML = "Age: " + data.results[0].dob.age
             gender.innerHTML = cityLocation + ", " + country
