62dca42b10e4397e5972598d92261e6aadc0b1f8
nruffin95 <nruffin95@gmail.com>
2022-04-26 21:20:55 -0500

    Added Final

--- test/js/script.js
+++ test/js/script.js
@@ -1,14 +1,31 @@
 const pushIt = document.getElementById('push')
+const getIt = document.getElementById('get')
+const gender = document.getElementById('gender')
+const activity = document.getElementById('activity')
 const button = document.getElementById('button1')
 
     const getRandomUserData = () => {
         fetch(`https://randomuser.me/api/`)
         .then(res => res.json())
         .then(data => {
-            pushIt.innerHTML = data.results[0].email
+            console.log(data)
+            pushIt.innerHTML = data.results[0].name.first
+            getIt.innerHTML = data.results[0].dob.age
+            gender.innerHTML = data.results[0].email
+        })
+    }
+
+    const getRandomActivity = () => {
+        fetch(`https://www.boredapi.com/api/activity/`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            activity.innerHTML = data.activity
         })
     }
 
    button.addEventListener("click", function() {
     getRandomUserData()
+    getRandomActivity()
   });
+
